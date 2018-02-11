655414313847dca9b25888e2d1da5f7e25620364
Sandeep Kunkunuru <sandeep.kunkunuru@ert.com>
2018-02-10 22:50:12 -0500

    ctri.nic.in revisited

---
+++ scripts/ctri.nic.in/catalog.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -ex
+
+# To Execute this script provide folder containing dataset as argument
+# ./ctri.nic.in/catalog.sh datainsights-results/ctri.nic.in/html datainsights-results/ctri.nic.in/analysis
+
+
+source $(pwd)/env.sh
+source $(pwd)/utility.sh
+
+OUT_DIR=${2:-'temp/ctri.nic.in'}
+
+cleanup  ${DATA_HOME}/${OUT_DIR}
+
+echo "S3FilePath|FileGUID|Source|Sector|Organization|ClinicalTrialID|TypeOfTrial|TypeOfStudy|StudyDesign|Sponsor|Title" \
+    > ${DATA_HOME}/${OUT_DIR}/studies.csv
+
+function analyse_file() {
+    index_name=`python -c "from sys import argv;print(hash(argv[1]))" ${3}`
+
+    content=$( grep -Pzo "<table align=center(\n|.)*</table>" ${1} | tr -s " \t\n\r" | tr -d "\t\n\r" )
+    content=$( echo ${content} | sed -e 's/<[^>]*>//g' | sed 's/&nbsp;//g'| tr -s " \t\n\r" | tr -d "\t\n\r")
+
+    ctr_num=$( grep -oE "CTRI/[^\[]*\[Registered" <<< ${content} || true)
+    ctr_num=${ctr_num//[Registered/}
+
+    type=$( grep -oE "Type of Trial (.)* Type of Study" <<< ${content} || true)
+    type=${type//Type of Trial /}
+    type=${type// Type of Study/}
+
+    typeOfStudy=$( grep -oE "Type of Study (.)* Study Design"<<< ${content}  || true)
+    typeOfStudy=${typeOfStudy//Type of Study /}
+    typeOfStudy=${typeOfStudy// Study Design/}
+
+    studyDesign=$( grep -oE "Study Design (.)* Public Title of Study" <<< ${content} || true)
+    studyDesign=${studyDesign//Study Design /}
+    studyDesign=${studyDesign// Public Title of Study/}
+
+    sponsor=$( grep -oE "Primary Sponsor (.)* Type of Sponsor" <<< ${content} || true)
+    sponsor=${sponsor//Primary Sponsor /}
+    sponsor=${sponsor// Type of Sponsor/}
+
+    title=$(grep -oE "Scientific Title of Study (.)* Secondary ID" <<< ${content} || true)
+    title=${title//Scientific Title of Study /}
+    title=${title// Secondary ID/}
+
+    entry="${3}|${index_name}|ctri.nic.in|Health|ICMR|${ctr_num}|${type}|${typeOfStudy}|${studyDesign}|${sponsor}|${title}"
+    entry=$(echo "${entry}" | xargs -0)
+    entry=${entry//$'\r'/}
+    entry=${entry//\"/}
+
+
+    echo ${entry} >> ${2}
+}
+
+
+## now loop through the above array
+ls ${DATA_HOME}/${1} | grep -oE "[^ ]*\.html" | while read f
+do
+    analyse_file ${DATA_HOME}/${1}/${f} ${DATA_HOME}/${OUT_DIR}/studies.csv  ${1}/${f}
+done
--- scripts/ctri.nic.in/trials.sh
+++ scripts/ctri.nic.in/trials.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # To Execute this script provide folder containing dataset as argument
-# ./ctri.nic.in/trials.sh datainsights-in/ctri.nic.in/ datainsights-results/ctri.nic.in/pdfs
+# ./ctri.nic.in/trials.sh datainsights-in/ctri.nic.in/ datainsights-results/ctri.nic.in/html
 
 
 source $(pwd)/env.sh
@@ -13,15 +13,15 @@ OUT_DIR=${2:-'temp/ctri.nic.in'}
 cleanup  ${DATA_HOME}/${OUT_DIR}
 
 
-prefix_url="http://ctri.nic.in/Clinicaltrials/pdf_generate.php?"
-suffix_url="&EncHid=&modid=&compid=%27,%272det%27"
+prefix_url="http://ctri.nic.in/Clinicaltrials/pmaindet2.php?"
+suffix_url=""
 
 function download_trial(){
     f=${1}
     f=${f//&/}
     g=${f/trialid=//}
     wget -q ${prefix_url}${f}${suffix_url} \
-         -O ${DATA_HOME}/${OUT_DIR}/${g}.pdf -o ${LOG_HOME}/${f}.pdf.log || true
+         -O ${DATA_HOME}/${OUT_DIR}/${g}.html -o ${LOG_HOME}/${f}.html.log || true
 }
 
 ## now loop through the above array
