355c663713057248aac96b3db5e3d494dd5a69b2
anil <panilkumar53@gmail.com>
2018-05-23 20:37:16 +0530

    removed  aws cli's and sed

--- scripts/athena/global/utdm_automation.sh
+++ scripts/athena/global/utdm_automation.sh
@@ -8,23 +8,21 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     --result-configuration OutputLocation=s3://datainsights-results/temp/utdm/ \
     --output text > temp.txt
 done < "$1"
-sleep 65
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 queryresultid="$line"
 done < temp.txt
+
 queryresultfile="$queryresultid$s3fileextention"
 s3fullpath="$s3path$queryresultfile"
-echo $s3fullpath
-count=$(aws s3 ls $s3fullpath | wc -l)
-echo $count
-if [ $count -gt 0 ]
-then
-   $(aws s3 cp $s3fullpath $(pwd))
-fi
 
-$(sed -i 's/"",""/""|""/g' $(pwd)/$queryresultfile)
-$(sed -i 's/","/"|"/g' $(pwd)/$queryresultfile)
-$(sed -i 's/,,/||/g' $(pwd)/$queryresultfile)
-$(sed -i 's/""|""/"",""/g' $(pwd)/$queryresultfile)
+sleep 60
 
-$(aws s3 cp $(pwd)/$queryresultfile s3://datainsights-results/temp/global/athena/ctd_utdm/temp.csv)
+aws s3 cp $s3fullpath $(pwd)
+
+sed -i 's/"",""/""|""/g' $(pwd)/$queryresultfile
+sed -i 's/","/"|"/g' $(pwd)/$queryresultfile
+sed -i 's/,,/||/g' $(pwd)/$queryresultfile
+sed -i 's/""|""/"",""/g' $(pwd)/$queryresultfile
+
+aws s3 cp $(pwd)/$queryresultfile s3://datainsights-results/temp/global/athena/ctd_utdm/global_registries.csv
