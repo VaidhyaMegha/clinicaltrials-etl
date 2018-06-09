fe4b33d8feda82c615164e51e6be726f471e0b2a
Sandeep Kunkunuru <sandeep@hsdlc.com>
2018-06-09 23:32:49 +0530

    WIP actrn. Cleanup for ctri

--- batch/ctri/keys.py
+++
@@ -1,6 +0,0 @@
-import json
-import collections
-
-kv = json.load(open('keys.json'), object_pairs_hook=collections.OrderedDict) # define desired replacements here
-
-print("s3_file_path~file_guid~source~sector~organization~" + "~".join(kv.keys()))
