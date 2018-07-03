3ae17b154f565454884dd3a102d68cd3fb42b0d5
Sandeep Kunkunuru <sandeep@hsdlc.com>
2018-07-03 16:52:23 +0530

    EUCTRN wip

--- batch/EUCTRN/html_json.sh
+++ batch/EUCTRN/html_json.sh
@@ -3,6 +3,8 @@ set -ex
 
 html_dir=${1}
 download=${2:-'no'}
+s3_bucket=${3:-'s3://hsdlc-results/euctrn-adapter/'}
+context_dir=${4:-'/usr/local/dataintegration'}
 prefix_url="https://www.clinicaltrialsregister.eu/"
 suffix_url="ctr-search/search?query=&dateFrom=2003-01-01&dateTo=2018-07-01&page="
 
@@ -16,15 +18,15 @@ function download_index_page(){
 }
 
 function download_and_analyse_trial(){
-    g=${1}
+    g=$( echo ${1//ctr-search/} | grep -oE "[0-9\-]*")
+
     wget -q ${prefix_url}${1} -O ${html_dir}/studies/${g}.html  || true
 
     cat ${html_dir}/studies/${g}.html  | pup 'div.detail :parent-of(td.cellGrey)  json{}' | \
       jq '.[] | {(.children[0].text): .children[1].text}' | jq -s add >> ${2}/${g}_1.json
 
-    cat htmls/studies/2013-001663-23.html  | pup 'div.detail tr.tricell json{}' | \
+    cat ${html_dir}/studies/${g}.html  | pup 'div.detail tr.tricell json{}' | \
       jq  '.[] | {(.children[1].text): .children[2].text}' | jq -s add >> ${2}/${g}_2.json
-
 
     jq -c -s '.[0] * .[1]' ${2}/${g}_1.json ${2}/${g}_2.json >> ${2}/studies.json
 
@@ -46,7 +48,7 @@ if [[ ${download} == 'yes' ]]; then
 
     NUM_OF_PAGES=`cat ${html_dir}/*.html | grep -oE '<a href=[^p]*page[^>]*>' | grep -oE '[0-9]*' | sort -u | head -n 1`
 
-    for i in {2..${NUM_OF_PAGES}}
+    for (( i=2; i<=${NUM_OF_PAGES}; i++ ))
     do
         download_index_page ${i}
     done
@@ -55,6 +57,12 @@ if [[ ${download} == 'yes' ]]; then
     do
         download_and_analyse_trial ${f} ${html_dir}/studies/json
     done
-#else
+
+    aws s3 sync  ${html_dir}/studies/ ${s3_bucket} --delete
+else
+    cat ${html_dir}/*.html | grep -oE "ctr-search\/trial\/[0-9\-]*/[A-Z][A-Z]" | while read f
+    do
+        download_and_analyse_trial ${f} ${html_dir}/studies/json
+    done
 
 fi
