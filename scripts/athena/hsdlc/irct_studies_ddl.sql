10876859710a7adf3d60e448907f057b021fa027
anil <panilkumar53@gmail.com>
2018-07-15 07:49:47 +0530

    ddl

---
+++ scripts/athena/hsdlc/irct_studies_ddl.sql
@@ -0,0 +1,48 @@
+ CREATE EXTERNAL TABLE `irct_studies`(
+ `Protocol_Summary` string,
+ `IRCT_RegistrationNumber` string,
+ `Registration` string,
+ `Registration_timing` string,
+ `Last_Update` string,
+ `UpdateCount` string,
+ `Registrant_Name_Of_Organization_or_Entity` string,
+ `Country` string,
+ `Phone` string,
+ `Email` string,
+ `Recruitment_status` string,
+ `Funding_source` string,
+ `Expected_Recruitment_start_Date` string,
+ `Expected_Recruitment_End_Date` string,
+ `Actual_recruitment_start_date` string,
+ `Actual_recruitment_End_date` string,
+ `Scientific_title` string,
+ `Public_title` string,
+ `InclusionCriteria` string,
+ `ExclusionCriteria` string,
+ `Age` string,
+ `Gender` string,
+ `phase` string,
+ `Groups_that_have_been_masked` string,
+ `Target_sample_size` string,
+ `Randomization_investigator_s_opinion` string,
+ `Randomization_description` string,
+ `Blinding_investigator_s_opinion` string,
+ `Blinding_description` string,
+ `Assignment` string,
+ `Secondary_Ids` Array<Map<string,string>>,
+ `Health_Conditions_studies` Array<Map<string,string>>,
+ `Primary_outcomes` Array<Map<string,string>>,
+ `Secondary_outcomes` Array<Map<string,string>>,
+ `Intervention_groups` Array<Map<string,string>>,
+ `Recruitment_centers` Array<Map<string,string>>,
+ `Sponsors__or_FundingSources` Array<Map<string,string>>,
+ `EthicsCommitte` Array<Map<string,string>>,
+ `PersonResponsibleForGeneralQueries` Array<Map<string,string>>,
+ `PersonResponsibleForScientificQueries` Array<Map<string,string>>,
+ `PersonResponsibleForUpdatingData` Array<Map<string,string>>
+)
+  ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+      LOCATION 's3://hsdlc-results/irct-adapter/json'
+      TBLPROPERTIES (
+      'ignore.malformed.json'= 'true'
+      );
