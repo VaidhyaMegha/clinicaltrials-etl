80a26190d9d82f41a25ed01ed67392a659d780ba
anil <anil@hsdlc.com>
2018-08-19 17:53:29 +0530

    etl added

---
+++ batch/perctr/etl/xml_json.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -ex
+
+xml_dir=${1}
+download=${2:-'no'}
+start_year=${3:-'1995'}
+end_year=${4:-`date +'%Y'`}
+s3_bucket=${5:-'s3://hsdlc-results/perct-adapter/'}
+context_dir=${6:-'/usr/local/dataintegration'}
+prefix_url="http://www.ins.gob.pe/ensayosclinicos/rpec/XML/"
+suffix_url="xmlall"
+
+function download_xml_page(){
+    wget  -q ${prefix_url}${suffix_url}${1}.xml \
+         -O ${xml_dir}/${1}.xml  || true
+}
+
+pushd ${context_dir}
+
+if [[ ${download} == 'yes' ]]; then
+
+  if [ -d ${xml_dir}/studies ]; then
+    rm -rf ${xml_dir}/studies
+  fi
+
+   mkdir -p ${xml_dir}/studies
+   mkdir ${xml_dir}/studies/json
+
+    for (( i=${start_year}; i<=${end_year}; i++ ))
+    do
+        download_xml_page ${i}
+    done
+
+    find ${xml_dir} -type f -name "*.xml" | while read f
+    do
+     cat ${f} | node ${xml_dir}/etl/html_json.js  | jq -c '.trials.trial[]' >>  ${xml_dir}/studies/json/studies.json
+    done
+
+    aws s3 sync  ${xml_dir} ${s3_bucket} --delete
+
+
+else
+
+    find ${xml_dir} -type f -name "*.xml" | while read f
+    do
+     cat ${f} | node ${xml_dir}/etl/html_json.js  | jq -c '.trials.trial[]' >>  ${xml_dir}/studies/json/studies.json
+    done
+
+    aws s3 sync  ${xml_dir} ${s3_bucket} --delete
+fi
+
+
+
+popd
