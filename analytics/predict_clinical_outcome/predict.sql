8d1fa7c1e68faf2e5fd5b99c3ded83266ed2c875
Sandeep Kunkunuru <sandeep@vaidhyamegha.com>
2019-01-12 13:24:31 +0530

    v7 with .718 F1 score

--- analytics/predict_clinical_outcome/predict.sql
+++ analytics/predict_clinical_outcome/predict.sql
@@ -109,6 +109,11 @@ select distinct why_stopped from ct_studies;
 https://console.aws.amazon.com/athena/query/results/06a736aa-dd99-4b4c-8563-200f54b1423f/csv
 aws s3 cp s3://default-query-results-519510601754-us-east-1/06a736aa-dd99-4b4c-8563-200f54b1423f.csv ~/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/
 
+select word, count(1) as cnt from ct_studies cross join unnest(split(why_stopped, ' ')) as i(word)
+where overall_status in ('Withdrawn', 'Terminated', 'Suspended') and length(word) > 6 group by word order by cnt desc;
+https://console.aws.amazon.com/athena/query/results/a10bcb18-bc6a-4e09-af85-7f577861f9fb/csv
+aws s3 cp s3://default-query-results-519510601754-us-east-1/a10bcb18-bc6a-4e09-af85-7f577861f9fb.csv ~/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/
+
 select p.mesh_term_condition, count(1) as cnt
 from ct_studies c cross join unnest(cast(json_extract(condition_browse, '$') as array(JSON))) as p(mesh_term_condition)
 group by mesh_term_condition
@@ -130,6 +135,7 @@ aws s3 cp s3://default-query-results-519510601754-us-east-1/3b876a3c-5151-4c00-b
 
 mvn exec:java -Dexec.args="/home/mahalaxmi/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/3b876a3c-5151-4c00-b7c8-8d4738a3d809.csv replaceDelimiter  /home/mahalaxmi/temp/temp3.csv"
 
+-- multiclass classification (https://console.aws.amazon.com/machinelearning/home?region=us-east-1#/predictor-insight/ml-NcemxohvNYA?tabId=summary)
 select cast(id_info as JSON), official_title, cast(study_design_info as JSON), cast(sponsors as JSON), overall_status,
 cast(enrollment as JSON), keyword, condition, cast(location as JSON), why_stopped, phase, study_type, number_of_arms,
 number_of_groups, condition_browse, intervention_browse, cast(primary_outcome as JSON), cast(secondary_outcome as JSON),
@@ -140,6 +146,7 @@ aws s3 cp s3://default-query-results-519510601754-us-east-1/fb5dfaee-ea61-47e6-b
 
 mvn exec:java -Dexec.args="/home/mahalaxmi/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/fb5dfaee-ea61-47e6-be3b-1fed5681364e.csv replaceDelimiter  /home/mahalaxmi/temp/temp4.csv"
 
+-- binary classification (https://console.aws.amazon.com/machinelearning/home?region=us-east-1#/predictor-insight/ml-wtrQLxxZVlX?tabId=summary)
 select id_info.nct_id as trial_id, official_title,phase, study_type, number_of_arms, number_of_groups,
 
 json_extract_scalar(json_parse(keyword), '$[1]') as first_keyword,
@@ -197,6 +204,7 @@ element_at(secondary_outcome, 3).measure as third_secondary_outcome_outcome_meas
 
 responsible_party.responsible_party_type as responsible_party_type,
 
+biospec_descr.textblock as biospec,
 eligibility.study_pop.textblock as eligibility_study_population,
 eligibility.sampling_method as eligibility_sampling_method,
 eligibility.gender_based as eligibility_gender_based,
@@ -218,7 +226,7 @@ https://console.aws.amazon.com/athena/query/results/f958bb55-06be-431c-ae33-0e06
 aws s3 cp s3://default-query-results-519510601754-us-east-1/f958bb55-06be-431c-ae33-0e06b30f5b3b.csv ~/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/
 
 
--- same as above
+-- Multi-Class Classification  (v3 in machinelearning - https://console.aws.amazon.com/machinelearning/home?region=us-east-1#/predictor-insight/ml-OJpWMzO47RJ?tabId=summary)
 
 select id_info.nct_id as trial_id, official_title,phase, study_type, number_of_arms, number_of_groups,
 
@@ -292,3 +300,105 @@ where overall_status in ('Completed', 'Approved for marketing', 'Withdrawn', 'Te
 
 https://console.aws.amazon.com/athena/query/results/0a8c8d78-4ccf-4da4-a3b6-1fcd5c368475/csv
 aws s3 cp s3://default-query-results-519510601754-us-east-1/0a8c8d78-4ccf-4da4-a3b6-1fcd5c368475.csv ~/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/
+
+
+-- binary classification ()
+select id_info.nct_id as trial_id, official_title,phase, study_type, number_of_arms, number_of_groups, target_duration,
+
+json_extract_scalar(json_parse(keyword), '$[1]') as first_keyword,
+json_extract_scalar(json_parse(keyword), '$[2]') as second_keyword,
+json_extract_scalar(json_parse(keyword), '$[3]') as third_keyword,
+json_extract_scalar(json_parse(keyword), '$[4]') as fourth_keyword,
+json_extract_scalar(json_parse(keyword), '$[5]') as fifth_keyword,
+
+json_extract_scalar(json_parse(condition_browse), '$[1]') as first_condition,
+json_extract_scalar(json_parse(condition_browse), '$[2]') as second_condition,
+json_extract_scalar(json_parse(condition_browse), '$[3]') as third_condition,
+json_extract_scalar(json_parse(condition_browse), '$[4]') as fourth_condition,
+json_extract_scalar(json_parse(condition_browse), '$[5]') as fifth_condition,
+
+json_extract_scalar(json_parse(intervention_browse), '$[1]') as first_intervention,
+json_extract_scalar(json_parse(intervention_browse), '$[2]') as second_intervention,
+json_extract_scalar(json_parse(intervention_browse), '$[3]') as third_intervention,
+json_extract_scalar(json_parse(intervention_browse), '$[4]') as fourth_intervention,
+json_extract_scalar(json_parse(intervention_browse), '$[5]') as fifth_intervention,
+
+study_design_info.allocation as allocation,
+study_design_info.intervention_model as intervention_model,
+study_design_info.primary_purpose as primary_purpose,
+study_design_info.observational_model as observational_model,
+study_design_info.time_perspective as time_perspective,
+study_design_info.masking as masking,
+sponsors.lead_sponsor.agency_class as lead_sponsor_agency_class,
+
+element_at(sponsors.collaborator, 1).agency_class as first_collaborator_agency_class,
+element_at(sponsors.collaborator, 2).agency_class as second_collaborator_agency_class,
+element_at(sponsors.collaborator, 3).agency_class as third_collaborator_agency_class,
+element_at(sponsors.collaborator, 4).agency_class as fourth_collaborator_agency_class,
+element_at(sponsors.collaborator, 5).agency_class as fifth_collaborator_agency_class,
+
+enrollment.type as enrollment_type,
+enrollment.text_node_value as enrollment_value,
+
+element_at(location, 1).facility.address.zip as first_location_zip,
+element_at(location, 1).facility.address.country as first_location_country,
+
+element_at(location, 2).facility.address.zip as second_location_zip,
+element_at(location, 2).facility.address.country as second_location_country,
+
+element_at(location, 3).facility.address.zip as third_location_zip,
+element_at(location, 3).facility.address.country as third_location_country,
+
+element_at(location, 4).facility.address.zip as fourth_location_zip,
+element_at(location, 4).facility.address.country as fourth_location_country,
+
+element_at(location, 5).facility.address.zip as fifth_location_zip,
+element_at(location, 5).facility.address.country as fifth_location_country,
+
+element_at(primary_outcome, 1).measure as first_primary_outcome_measure,
+element_at(primary_outcome, 2).measure as second_primary_outcome_measure,
+element_at(primary_outcome, 3).measure as third_primary_outcome_measure,
+element_at(primary_outcome, 4).measure as fourth_primary_outcome_measure,
+element_at(primary_outcome, 5).measure as fifth_primary_outcome_measure,
+
+element_at(secondary_outcome, 1).measure as first_secondary_outcome_measure,
+element_at(secondary_outcome, 2).measure as second_secondary_outcome_measure,
+element_at(secondary_outcome, 3).measure as third_secondary_outcome_measure,
+element_at(secondary_outcome, 4).measure as fourth_secondary_outcome_measure,
+element_at(secondary_outcome, 5).measure as fifth_secondary_outcome_measure,
+
+element_at(other_outcome, 1).measure as first_other_outcome_measure,
+element_at(other_outcome, 2).measure as second_other_outcome_measure,
+element_at(other_outcome, 3).measure as third_other_outcome_measure,
+element_at(other_outcome, 4).measure as fourth_other_outcome_measure,
+element_at(other_outcome, 5).measure as fifth_other_outcome_measure,
+
+eligibility.sampling_method as eligibility_sampling_method,
+eligibility.gender as eligibility_gender,
+eligibility.minimum_age as eligibility_minimum_age,
+eligibility.maximum_age as eligibility_maximum_age,
+eligibility.healthy_volunteers as eligibility_healthy_volunteers,
+
+
+oversight_info.has_dmc has_dmc,
+oversight_info.is_fda_regulated_drug is_fda_regulated_drug,
+oversight_info.is_fda_regulated_device is_fda_regulated_device,
+oversight_info.is_unapproved_device is_unapproved_device,
+oversight_info.is_ppsd is_ppsd,
+oversight_info.is_us_export is_us_export,
+
+responsible_party.responsible_party_type as responsible_party_type,
+responsible_party.investigator_affiliation as investigator_affiliation,
+responsible_party.invewastigator_title as invewastigator_title,
+
+case when overall_status = 'Completed' then 'Completed'
+     when overall_status = 'Approved for marketing' then 'Completed'
+     else 'Did not complete'
+     end as status
+
+from hsdlc.ct_studies
+where overall_status in ('Completed', 'Approved for marketing', 'Withdrawn', 'Terminated', 'Suspended') ;
+
+
+https://console.aws.amazon.com/athena/query/results/5e7a096a-0619-46d5-9e38-a41eccf96fa1/csv
+aws s3 cp s3://default-query-results-519510601754-us-east-1/5e7a096a-0619-46d5-9e38-a41eccf96fa1.csv ~/projects/ctd/DI_ETL/analytics/predict_clinical_outcome/datasets/
