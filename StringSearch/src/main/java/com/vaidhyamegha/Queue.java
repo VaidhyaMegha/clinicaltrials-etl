17076e43d6ef23fd6289c39606f7ba98d6a8ddbf
Sandeep Kunkunuru <sandeep@vaidhyamegha.com>
2019-02-11 01:40:27 +0530

    Simplify Queue implementation

--- StringSearch/src/main/java/com/vaidhyamegha/Queue.java
+++ StringSearch/src/main/java/com/vaidhyamegha/Queue.java
@@ -1,109 +1,50 @@
 package com.vaidhyamegha;
 
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Queue<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of queue
-    private Node<Item> last;     // end of queue
-    private int n;               // number of elements on queue
+class Queue<T> implements Iterable<T> {
+    private Node<T> first;  
+    private Node<T> last;    
 
-    private static class Node<Item> {
-        private Item item;
-        private Node<Item> next;
+    private static class Node<Q> {
+        private Q q;
+        private Node<Q> next;
     }
 
-    public Queue() {
+    Queue() {
         first = null;
         last = null;
-        n = 0;
     }
 
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public int size() {
-        return n;
-    }
-
-    public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        return first.item;
-    }
-
-    public void enqueue(Item item) {
-        Node<Item> oldlast = last;
-        last = new Node<Item>();
-        last.item = item;
+    void enqueue(T t) {
+        Node<T> oldLast = last;
+        last = new Node<>();
+        last.q = t;
         last.next = null;
-        if (isEmpty()) first = last;
-        else oldlast.next = last;
-        n++;
+        if (first == null) first = last;
+        else oldLast.next = last;
     }
 
-    public Item dequeue() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        Item item = first.item;
-        first = first.next;
-        n--;
-        if (isEmpty()) last = null;   // to avoid loitering
-        return item;
+    public Iterator<T> iterator() {
+        return new ListIterator<>(first);
     }
 
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this) {
-            s.append(item);
-            s.append(' ');
-        }
-        return s.toString();
-    }
+    private class ListIterator<R> implements Iterator<R> {
+        private Node<R> current;
 
-    public Iterator<Item> iterator() {
-        return new ListIterator<Item>(first);
-    }
+        public ListIterator(Node<R> first) { current = first; }
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
+        public boolean hasNext() { return current != null; }
 
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
+        public void remove() { throw new UnsupportedOperationException(); }
 
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        public Item next() {
+        public R next() {
             if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
+            R r = current.q;
             current = current.next;
-            return item;
+            return r;
         }
     }
 
-
-    /**
-     * Unit tests the {@code Queue} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        Queue<String> queue = new Queue<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-"))
-                queue.enqueue(item);
-            else if (!queue.isEmpty())
-                StdOut.print(queue.dequeue() + " ");
-        }
-        StdOut.println("(" + queue.size() + " left on queue)");
-    }
 }
