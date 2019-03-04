189d48cd18f210aeca8eb5c45bd2b9347dedcab1
Sandeep Kunkunuru <sandeep@vaidhyamegha.com>
2019-03-05 00:14:30 +0530

    tctr : process 1 zip file completely before taking next one

--- batch/tctr/xml_json.sh
+++ batch/tctr/xml_json.sh
@@ -39,7 +39,7 @@ if [[ ${download} == 'yes' ]]; then
         if [[ -s ${xml_dir}/studies/xml/${h} ]]
         then
             g=${i//\.xml/\.new}
-            iconv -f us-ascii -t UTF-8//TRANSLIT ${xml_dir}/studies/xml/${h} -o ${g}
+            iconv -f us-ascii -t UTF-8//TRANSLIT ${xml_dir}/studies/xml/${h} -o ${g} || true
             cat ${g} | node ${context_dir}/etl/xml_json.js  | jq -c '.trials.trial[]' >>  ${xml_dir}/studies/json/studies.json
             rm -f ${g}
         fi
