eed5ab0c7abf7a5ae606a74997b6f742db0ccbd2
madhavi <madhavi@Vaidhyamegha.com>
2019-03-08 17:32:37 +0530

    stdout and stderror in log file

--- batch/jprn/driving.sh
+++ batch/jprn/driving.sh
@@ -4,7 +4,7 @@ set -x
 NOW=$(date +%y-%m-%d-%H:%M:%S)
 LOGFILE='LOG-'$NOW
 context_dir=${3:-'/usr/local/dataintegration'}
-s3_bucket_log=${4:-'s3://hsdlc-results/tctr-adapter/logs/'}
+s3_bucket_log=${4:-'s3://hsdlc-results/rapid-prototype/'}
 
 bash ./html_json.sh 'htmls' 'yes' > $context_dir/$LOGFILE.log 2>&1
 
