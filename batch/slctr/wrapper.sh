d4e0b2a7443ec3de7437266628438e5be5dce151
madhavi <madhavi@Vaidhyamegha.com>
2019-03-14 17:03:58 +0530

    adding set -e and removing Makefile for slctr

--- batch/slctr/wrapper.sh
+++ batch/slctr/wrapper.sh
@@ -9,8 +9,14 @@ s3_bucket_log=${4:-'s3://hsdlc-results/rapid-prototype/'}
 pushd ${context_dir}
 
 bash ./html_json.sh $1 $2 > $context_dir/$LOGFILE.log 2>&1
+if [ $? -eq 0 ]; then
+  aws s3 cp $context_dir/$LOGFILE.log ${s3_bucket_log}
+else
+  aws s3 cp $context_dir/$LOGFILE.log ${s3_bucket_log}
+  set -e
+ fi
 
-aws s3 cp $context_dir/$LOGFILE.log ${s3_bucket_log}
+
 popd
 
 
